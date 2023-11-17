cf1f7c7f7be6580e4777275ca02666e6f326f6ce
Alvaro Fragoso <alvarofragosoc@hotmail.com>
2023-11-17 13:02:39 +0000

    added analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://4mentor.com",
-  integrations: [tailwind(), astroI18next(), react(), sitemap()]
+  integrations: [tailwind(), astroI18next(), react(), sitemap()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
